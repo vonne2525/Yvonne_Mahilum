08ffd372c6bd130113793b4d58610cee935c4afa
vonne2525 <ymahilum252006@gmail.com>
2026-09-19 23:58:28 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to my first API!"
+
+
+@app.route('/student')
+def get_student():
+    return jsonify({
+        "student_id": "24-0087",
+        "name": "Kc Jane Marie Ladoing",
+        "program": "BSIT",
+        "year": 3,
+        "section": "B"
+    })
+
+
+@app.route('/hello')
+def say_hello():
+    name = request.args.get('name', 'Student')
+    return jsonify({
+        "message": f"Hello, {name}!"
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
